b59564ecbe5a2c3a1bf65127000e93e6217bb7d7
Enger Colman <engerterseer@gmail.com>
2020-04-12 21:49:35 +0100

    Challenge 1 - Completed

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,21 @@
-const covid19ImpactEstimator = (data) => data;
+const getInfectedOverTimeSpan = (currentlyInfected, factor) => currentlyInfected * (2 ** factor);
+
+const covid19ImpactEstimator = (data) => {
+  const { reportedCases } = data;
+
+  const impact = {};
+  impact.currentlyInfected = reportedCases * 10;
+  impact.infectionsByRequestedTime = getInfectedOverTimeSpan(impact.currentlyInfected, 10);
+
+  const severeImpact = {};
+  severeImpact.currentlyInfected = reportedCases * 50;
+  impact.infectionsByRequestedTime = getInfectedOverTimeSpan(severeImpact.currentlyInfected, 10);
+
+  return ({
+    data,
+    impact,
+    severeImpact
+  });
+};
 
 export default covid19ImpactEstimator;
