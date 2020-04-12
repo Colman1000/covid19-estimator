c2e6015de11b657b2b2cac8b65b56bed0a2d2953
Enger Colman <engerterseer@gmail.com>
2020-04-12 22:59:38 +0100

    Challenge 1 - Updated

--- src/estimator.js
+++ src/estimator.js
@@ -1,15 +1,15 @@
 const trimNum = (number) => parseInt(number, 10);
-// eslint-disable-next-line max-len
-const getInfectedOverTimeSpan = (currentlyInfected, factor) => trimNum(currentlyInfected * (2 ** factor));
-
 const periodMap = {
   days: 1,
   weeks: 7,
   months: 30
 };
-
-// eslint-disable-next-line no-unused-vars
 const getTotalNumberOfDays = (periodElapsed, periodType) => periodElapsed * periodMap[periodType];
+
+const getInfectedOverTimeSpan = (currentlyInfected, { timeToElapse, periodType }) => {
+  const factor = trimNum(getTotalNumberOfDays(timeToElapse, periodType) / 3);
+  return trimNum(currentlyInfected * (2 ** factor));
+};
 
 /*
 Data Structure
@@ -31,11 +31,11 @@ const covid19ImpactEstimator = (data) => {
 
   const impact = {};
   impact.currentlyInfected = trimNum(reportedCases * 10);
-  impact.infectionsByRequestedTime = getInfectedOverTimeSpan(impact.currentlyInfected, 9);
+  impact.infectionsByRequestedTime = getInfectedOverTimeSpan(impact.currentlyInfected, data);
 
   const severeImpact = {};
   severeImpact.currentlyInfected = trimNum(reportedCases * 50);
-  impact.infectionsByRequestedTime = getInfectedOverTimeSpan(severeImpact.currentlyInfected, 9);
+  impact.infectionsByRequestedTime = getInfectedOverTimeSpan(severeImpact.currentlyInfected, data);
 
   return ({
     data,
