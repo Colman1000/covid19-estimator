b047923362f1286fb882c6c9f2bc1a2742c7de22
Enger Colman <engerterseer@gmail.com>
2020-04-13 19:46:28 +0100

    Challenge 3 - Done

--- src/estimator.js
+++ src/estimator.js
@@ -10,23 +10,49 @@ const getInfectedOverTimeSpan = (currentlyInfected, { timeToElapse, periodType }
   const factor = trimNum(getTotalNumberOfDays(timeToElapse, periodType) / 3);
   return trimNum(currentlyInfected * (2 ** factor));
 };
+const getNumOfAvailBeds = ({ totalHospitalBeds }) => Math.ceil(totalHospitalBeds * 0.35);
+const getDollarsInFlight = (infectionsByRequestedTime, { timeToElapse, periodType, region }) => {
+  const timeSpan = getTotalNumberOfDays(timeToElapse, periodType);
+  const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = region;
+  return avgDailyIncomeInUSD * avgDailyIncomePopulation * timeSpan;
+};
 
 const covid19ImpactEstimator = (data) => {
   const { reportedCases } = data;
 
+  const availableBeds = getNumOfAvailBeds(data);
+
+
   const impactCurrentlyInfected = trimNum(reportedCases * 10);
+  const iInfectionsByRequestedTime = getInfectedOverTimeSpan(impactCurrentlyInfected, data);
+  const iSevereCasesByRequestedTime = trimNum(iInfectionsByRequestedTime * 0.15);
+  const iHospitalBedsByRequestedTime = availableBeds - iSevereCasesByRequestedTime;
+
 
   const severeImpactCurrentlyInfected = trimNum(reportedCases * 50);
+  const sInfectionsByRequestedTime = getInfectedOverTimeSpan(severeImpactCurrentlyInfected, data);
+  const sSevereCasesByRequestedTime = trimNum(sInfectionsByRequestedTime * 0.15);
+  const sHospitalBedsByRequestedTime = availableBeds - sSevereCasesByRequestedTime;
 
   return ({
     data,
     impact: {
       currentlyInfected: impactCurrentlyInfected,
-      infectionsByRequestedTime: getInfectedOverTimeSpan(impactCurrentlyInfected, data)
+      infectionsByRequestedTime: iInfectionsByRequestedTime,
+      severeCasesByRequestedTime: iSevereCasesByRequestedTime,
+      hospitalBedsByRequestedTime: iHospitalBedsByRequestedTime,
+      casesForICUByRequestedTime: trimNum(iInfectionsByRequestedTime * 0.05),
+      casesForVentilatorsByRequestedTime: trimNum(iInfectionsByRequestedTime * 0.02),
+      dollarsInFlight: getDollarsInFlight(iInfectionsByRequestedTime, data)
     },
     severeImpact: {
       currentlyInfected: severeImpactCurrentlyInfected,
-      infectionsByRequestedTime: getInfectedOverTimeSpan(severeImpactCurrentlyInfected, data)
+      infectionsByRequestedTime: sInfectionsByRequestedTime,
+      severeCasesByRequestedTime: sSevereCasesByRequestedTime,
+      hospitalBedsByRequestedTime: sHospitalBedsByRequestedTime,
+      casesForICUByRequestedTime: trimNum(sInfectionsByRequestedTime * 0.05),
+      casesForVentilatorsByRequestedTime: trimNum(sInfectionsByRequestedTime * 0.02),
+      dollarsInFlight: getDollarsInFlight(sSevereCasesByRequestedTime, data)
     }
   });
 };
